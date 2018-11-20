f90600ebb6c2201989394119249b2be76ec43a9e
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-20 01:38:25 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ end
 
 
 
-def introduction_with_language(name, language)
-    puts 'Hi, my name is #{name} and I am learning to program in #{language}.'
+def introduction_with_language(na, language)
+    puts 'Hi, my name is #{na} and I am learning to program in #{language}.'
 end  
